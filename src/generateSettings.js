12b13aa2747814cbb9eecea26e780a0c10017adb
Simen Daehlin <simen.dehlin@gmail.com>
2020-01-18 23:26:06 +0000

    Now generating correct settings depending on OS

--- src/generateSettings.js
+++ src/generateSettings.js
@@ -1,51 +1,45 @@
 import { platform } from 'os';
-
+const userOS = getOS();
 export async function generatePythonSettings(config) {
-  return `{
-		"python.pythonPath": ${generateOsVariable(platform, config.envName).path},
-		"python.terminal.activateEnvironment": true,
-		"python.linting.enabled": true,
-		"python.linting.pylintArgs": ["--load-plugins=pylint_${pylintValue(
-      options.template,
-    )}"],
-		"files.autoSave": "onFocusChange",
-		"terminal.integrated.env.${
-      generateOsVariable(platform, config.envName).name
-    }": {
-			"SECRET_KEY": "${Math.random()
+  const envPath = config.env ? generatePath(config) : '';
+  const settings = `{
+  ${envPath}
+  "python.terminal.activateEnvironment": true,
+  "python.linting.enabled": true,
+  "python.linting.pylintArgs": ["--load-plugins=pylint_${
+    config.template.django ? 'django' : 'flask'
+  }"],
+  "files.autoSave": "onFocusChange",
+  "terminal.integrated.env.${userOS}": {
+    "SECRET_KEY": "${Math.random()
+      .toString(36)
+      .substring(2, 15) +
+      Math.random()
         .toString(36)
-        .substring(2, 15) +
-        Math.random()
-          .toString(36)
-          .substring(2, 15)}",
-				"DEV": "1",
-				"FLASK_DEBUG": "1",
-				"MONGO_URI": "YOUR MONGO URI GOES HERE"
-			}
-		}`;
+        .substring(2, 15)}",
+      "DEV": "1",
+      "FLASK_DEBUG": "1",
+      "MONGO_URI": "YOUR MONGO URI GOES HERE"
+    }
+  }`;
+  return settings;
 }
 
-function generateOsVariable(platform, envName) {
-  console.log(platform);
-  const os = {
-    name: platform,
-    path: '',
-  };
-  if (platform === 'win32') {
-    os.name = 'windows';
-    os.path = `"\${workspaceFolder}\\${envName}\\bin\\python.exe"`;
-  } else if (platform === 'darwin') {
-    os.name = 'osx';
-    os.path = `"\${workspaceFolder}/${envName}/bin/python"`;
-  } else {
-    os.platform = 'linux';
-    os.path = `"\${workspaceFolder}/${envName}/bin/python"`;
+function generatePath(config) {
+  if (platform() === 'darwin' || platform() === 'linux')
+    return `"python.pythonPath:\${workspaceFolder}/${config.envName}/bin/python",`;
+  if (platform() === 'windows')
+    return `"python.pythonPath": "\${workspaceFolder}\\\\${config.envName}\\\\bin\\\\python.exe",`;
+}
+function getOS() {
+  switch (platform()) {
+    case 'win32':
+      return 'windows';
+    case 'darwin':
+      return 'osx';
+    case 'linux':
+      return 'linux';
+    default:
+      return false;
   }
-  return os;
-}
-
-function pylintValue(value) {
-  if (value === 'fsf') {
-    return 'django';
-  } else return 'flask';
 }
