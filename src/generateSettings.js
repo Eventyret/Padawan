51ed86e9a9a8d8983a89bf50b13cb6e371c14cf2
Simen Daehlin <simen.dehlin@gmail.com>
2020-01-18 20:10:19 +0000

    Generate dynamic config for vscode

--- src/generateSettings.js
+++ src/generateSettings.js
@@ -1,14 +1,17 @@
 import { platform } from 'os';
+
 export async function generatePythonSettings(config) {
   return `{
-		"python.pythonPath": ${generateOsVariable(platform)},
+		"python.pythonPath": ${generateOsVariable(platform, config.envName).path},
 		"python.terminal.activateEnvironment": true,
 		"python.linting.enabled": true,
 		"python.linting.pylintArgs": ["--load-plugins=pylint_${pylintValue(
       options.template,
     )}"],
 		"files.autoSave": "onFocusChange",
-		"terminal.integrated.env.osx": {
+		"terminal.integrated.env.${
+      generateOsVariable(platform, config.envName).name
+    }": {
 			"SECRET_KEY": "${Math.random()
         .toString(36)
         .substring(2, 15) +
@@ -22,16 +25,23 @@ export async function generatePythonSettings(config) {
 		}`;
 }
 
-function generateOsVariable(os) {
-  console.log(os);
-  switch (os) {
-    case 'win32':
-      return `"\${workspaceFolder}\\${config.envName}\\bin\\python.exe"`;
-    case 'dawin':
-      return `"\${workspaceFolder}/${config.envName}/bin/python"`;
-    case 'linux':
-      return `"\${workspaceFolder}/${config.envName}/bin/python"`;
+function generateOsVariable(platform, envName) {
+  console.log(platform);
+  const os = {
+    name: platform,
+    path: '',
+  };
+  if (platform === 'win32') {
+    os.name = 'windows';
+    os.path = `"\${workspaceFolder}\\${envName}\\bin\\python.exe"`;
+  } else if (platform === 'darwin') {
+    os.name = 'osx';
+    os.path = `"\${workspaceFolder}/${envName}/bin/python"`;
+  } else {
+    os.platform = 'linux';
+    os.path = `"\${workspaceFolder}/${envName}/bin/python"`;
   }
+  return os;
 }
 
 function pylintValue(value) {
