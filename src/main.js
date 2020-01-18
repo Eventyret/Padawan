f8f46e36410762d8862bd45c3eb76c372a0550d0
Simen Daehlin <simen.dehlin@gmail.com>
2020-01-18 18:20:37 +0000

    Added errorhandling for git

--- src/main.js
+++ src/main.js
@@ -33,10 +33,14 @@ async function createProjectDir(options) {
 }
 
 async function initGit(options) {
-  await git.cwd(options.targetDirectory);
-  await git.init();
-  await git.add('.');
-  await git.commit('Initial commit made by Padwan Tool');
+  try {
+    await git.cwd(options.targetDirectory);
+    await git.init();
+    await git.add('.');
+    await git.commit('Initial commit made by Padwan Tool');
+  } catch (err) {
+    console.error(err.message);
+  }
   return;
 }
 async function writeReadme(options) {
