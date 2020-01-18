a5cc886f286aa9624f678ec6b61808ecd3cc9c57
Simen Daehlin <simen.dehlin@gmail.com>
2020-01-18 15:16:36 +0000

    Added correct questions

---
+++ src/cli.js
@@ -0,0 +1,96 @@
+import arg from 'arg';
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+import clear from 'clear';
+import figlet from 'figlet';
+
+function parseArgumentsIntoOptions(rawArgs) {
+  const args = arg(
+    {
+      '--git': Boolean,
+      '--skip': Boolean,
+      '--install': Boolean,
+      '-g': '--git',
+      '-y': '--yes',
+      '-i': '--install',
+    },
+    {
+      argv: rawArgs.slice(2),
+    },
+  );
+  return {
+    skipPrompts: args['--skip'] || false,
+    git: args['--git'] || false,
+    template: args._[0],
+    runInstall: args['--install'] || false,
+  };
+}
+async function promptForMissingOptions(options) {
+  const defaultTemplate = 'ms1';
+  if (options.skipPrompts) {
+    return {
+      ...options,
+      template: options.template || defaultTemplate,
+    };
+  }
+
+  const questions = [];
+  if (!options.template) {
+    questions.push({
+      type: 'list',
+      name: 'template',
+      message: 'What milestone will you be working on?',
+      choices: [
+        {
+          name: 'User Centric Frontend (MS1)',
+          value: 'ms1',
+        },
+        {
+          name: 'Interactive Frontend (MS2)',
+          value: 'ms2',
+        },
+        {
+          name: 'Data Centric Development (MS3)',
+          value: 'ms3',
+        },
+        {
+          name: 'Full Stack Frameworks (MS4)',
+          value: 'ms4',
+        },
+      ],
+      default: defaultTemplate,
+    });
+  }
+
+  if (!options.git) {
+    questions.push({
+      type: 'confirm',
+      name: 'git',
+      message: 'Initialize a git repository?',
+      default: false,
+    });
+  }
+
+  const answers = await inquirer.prompt(questions);
+  return {
+    ...options,
+    template: options.template || answers.template,
+    git: options.git || answers.git,
+  };
+}
+
+function title() {
+  console.log(
+    chalk.yellow(
+      figlet.textSync('Padawan', { horizontalLayout: 'full', font: 'Big' }),
+    ),
+  );
+}
+
+export async function cli(args) {
+  clear();
+  title();
+  let options = parseArgumentsIntoOptions(args);
+  options = await promptForMissingOptions(options);
+  console.log(options);
+}
