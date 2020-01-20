d605d3029ae7495b8978ed66f203e841cec8d648
Simen Daehlin <simen.dehlin@gmail.com>
2020-01-20 19:54:03 +0000

    Default app.py added

--- templates/dcd/app.py
+++ templates/dcd/app.py
@@ -1,6 +1,19 @@
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
+from os import path
+if path.exists("env.py"):
+  import env 
+SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('MONGO_DBNAME')
+SECRET_KEY = os.environ.get('MONGO_URI')
+
+APP = Flask(__name__)
+
+@APP.route('/')
 def hello_world():
     return 'Hello, World!'
+
+if __name__ == '__main__':
+    APP.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            os.environ.get('DEBUG'))
