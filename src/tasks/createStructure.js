86b11cad21fe55b115550f8c19ac870d89ba0a55
Simen Daehlin <simen.dehlin@gmail.com>
2020-01-20 23:34:43 +0000

    Made folder structure file

---
+++ src/tasks/createStructure.js
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import ncp from 'ncp';
+import path from 'path';
+import { promisify } from 'util';
+
+const mkdir = promisify(fs.mkdir);
+const copy = promisify(ncp);
+
+export async function copyTemplateFiles(options) {
+  return copy(options.templateDirectory, options.targetDirectory, {
+    clobber: false,
+  });
+}
+export async function copyCommonFiles(options) {
+  return copy(options.commonDir, options.targetDirectory, {
+    clobber: false,
+  });
+}
+
+export async function createProjectDir(options) {
+  options.targetDirectory = path.resolve(
+    process.cwd(),
+    options.name.replace(/\s+/g, '-').toLowerCase(),
+  );
+
+  return mkdir(options.targetDirectory);
+}
