f0e71185460b970a0ca20400c718e788af43cecc
Simen Daehlin <simen.daehlin@ageas.co.uk>
2020-01-21 17:57:20 +0000

    Linted and started working on a PipeOut

--- src/tasks/virtualenv.js
+++ src/tasks/virtualenv.js
@@ -1,11 +1,17 @@
-const execa = require('execa');
+import execa from 'execa';
+import path from 'path';
 
 const subprocess = execa('python', ['-V']);
-const pythonInstall = execa('pip uninstall Flask',['--y']);
+
+const pythonInstall = execa('pip freeze > test.txt');
 
 (async () => {
   const { stdout } = await subprocess;
   console.log(stdout.substr(7));
+})();
+
+async function pipOutPut(options) {
+  options.targetDirectory = path.resolve(process.cwd());
   const test = await pythonInstall;
   console.log(test);
-})();
+}
