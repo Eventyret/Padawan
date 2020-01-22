3e70a8bd1c34a8d0b71c33bc673cbf62e9693c91
Simen Daehlin <simen.dehlin@gmail.com>
2020-01-22 00:31:05 +0000

    WIP - Now creates a virtualenv

--- src/tasks/virtualenv.js
+++ src/tasks/virtualenv.js
@@ -1,17 +1,40 @@
 import execa from 'execa';
-import path from 'path';
+import { platform } from 'os';
 
-const subprocess = execa('python', ['-V']);
+async function activate(options) {
+  const targetDir = options.targetDirectory;
+  let path;
+  let osVar;
+  let pythonExecutable;
+  let pip;
+  let requirements;
+  if (platform() == 'win32') {
+    //prettier-ignore
+    path = `\\env\\Scripts\\activate`;
+    osVar = '\\env';
+    pythonExecutable = '\\env\\Scripts\\python.exe';
+    pip = '\\env\\Scripts\\pip.exe';
+    requirements = '\\requirements.txt';
+  } else {
+    path = `/env/bin/activate`;
+    osVar = '/env';
+    pythonExecutable = 'env/bin/python';
+    pip = 'env/bin/pip';
+    requirements = '/requirements.txt';
+  }
+  await execa(`virtualenv ${targetDir}${osVar}`);
+  await execa(`${targetDir}${pip}`, ['install', `-r ${targetDir}${requirements}`]);
+  await execa(
+    `${targetDir}${pip}`, [`freeze --local >> ${targetDir}${requirements}`],
+  );
+}
 
-const pythonInstall = execa('pip freeze > test.txt');
-
-(async () => {
-  const { stdout } = await subprocess;
-  console.log(stdout.substr(7));
-})();
-
-async function pipOutPut(options) {
-  options.targetDirectory = path.resolve(process.cwd());
-  const test = await pythonInstall;
-  console.log(test);
+export async function pipOutPut(options) {
+  try {
+    await execa('pip install virtualenv');
+    const test = await activate(options);
+    console.log(test);
+  } catch (err) {
+    throw err;
+  }
 }
