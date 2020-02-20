cdef825482a6da4855b308262557128f01d45387
Simen Daehlin <simen.dehlin@gmail.com>
2020-02-20 20:31:03 +0000

    Fixed #5

--- src/generate/generateENV.js
+++ src/generate/generateENV.js
@@ -1,15 +1,17 @@
+// prettier-ignore
 export function generateENVFile() {
   return `
- os.environ["MONGO_URI"] = "YOUR MONGO URI GOES HERE"
- os.environ["MONGO_DBNAME"] = "YOUR MONGO DB NAME GOES HERE"
- os.environ["SECRET_KEY"] =  "${Math.random()
+import os
+
+os.environ["MONGO_URI"] = "YOUR MONGO URI GOES HERE"
+os.environ["MONGO_DBNAME"] = "YOUR MONGO DB NAME GOES HERE"
+os.environ["SECRET_KEY"] =  "${Math.random()
    .toString(36)
    .substring(2, 15) +
    Math.random()
      .toString(36)
-     .substring(2, 15)}",
-os.environ["DEV"]: "1",
-os.environ["DEBUG"]: "1",
+     .substring(2, 15)}"
+os.environ["DEBUG"]: "1"
 os.environ["IP"] = "0.0.0.0"
-os.environ["PORT"] = 5000`; // jshint ignore:line
+os.environ["PORT"] = 5000` // jshint ignore:line
 }
