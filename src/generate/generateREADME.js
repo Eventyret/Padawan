1eeb97a0ae79b2b08cc12fc2952346a2d6253976
Simen Daehlin <simen.daehlin@ageas.co.uk>
2020-02-21 09:24:41 +0000

    Created markdown file to hold testing and readme

--- src/generate/generateREADME.js
+++
@@ -1,8 +0,0 @@
-export async function generateREADME(config) {
-  return `
-  # ${options.name}
-  <div align="center">
-  <img src="https://upload.wikimedia.org/wikipedia/en/d/d7/Ahsoka_Tano.png" height="350" width="350">
-</div>
-  `
-}
