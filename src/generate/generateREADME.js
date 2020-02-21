2d23a70530a4cd41f39259bdc5a0ba8823f3ed47
Simen Daehlin <simen.daehlin@ageas.co.uk>
2020-02-21 08:55:49 +0000

    Started working on generating a readme

---
+++ src/generate/generateREADME.js
@@ -0,0 +1,8 @@
+export async function generateREADME(config) {
+  return `
+  # ${options.name}
+  <div align="center">
+  <img src="https://upload.wikimedia.org/wikipedia/en/d/d7/Ahsoka_Tano.png" height="350" width="350">
+</div>
+  `
+}
