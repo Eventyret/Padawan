7578ac7091d3cfd5198aeaf96dd9029e10555d0e
Eventyret <simen.dehlin@gmail.com>
2020-05-01 16:10:43 +0100

    Now creating virtualenv on unix

--- src/tasks/virtualenv.js
+++ src/tasks/virtualenv.js
@@ -1,12 +1,17 @@
 import execa from 'execa';
+import { asyncExec } from 'async-shelljs';
 import { getOS } from '../common/common';
 
 const target = {};
 let targetDir;
 async function activate(options) {
-  await execa(`virtualenv ${targetDir}${target.osVar}`);
-  await execa(`${targetDir}${target.pip}`, [`install`, `-r`, `${options.backendDir}${target.requirements}`]);
-  await execa(`${targetDir}${target.pip}`, ['freeze', '--local', '>>', `${targetDir}${target.requirements}`]);
+  try {
+    await asyncExec(`virtualenv ${targetDir}${target.osVar}`);
+    await asyncExec(`${targetDir}${target.pip} install -r ${options.backendDir}${target.requirements} > /dev/null 2>&1`);
+    await asyncExec(`${targetDir}${target.pip} freeze --local >> ${options.backendDir}${target.requirements} > /dev/null 2>&1`);
+  } catch(error) {
+    console.log(error, "Problem in Activate Function")
+  }
 }
 
 export async function pipOutPut(options) {
@@ -14,26 +19,26 @@ export async function pipOutPut(options) {
   try {
     const usrOS = await getOS();
     await os(options, usrOS);
-    if (!options.gitpod && usrOS === "windows") {
+    if (!options.gitpod && usrOS === 'windows') {
       await execa('pip install virtualenv');
       await activate(options);
-    } else {    
-      await execa('pip3', ['install', 'virtualenv'])
+    } else {
+      await asyncExec('pip3 install virtualenv > /dev/null 2>&1');
       await activate(options);
     }
     options.env = true;
   } catch (err) {
-    throw err;
+    console.log("This is a caught error")
   }
 }
 
 export async function flaskApp() {
-  await execa(`${targetDir}${target.pip}`, ['install', 'Flask']);
+  await asyncExec(`${targetDir}${target.pip} install Flask > /dev/null 2>&1`);
   return;
 }
 
 export async function djangoApp() {
-  await execa(`${targetDir}${target.pip}`, ['install', 'Django']);
+  await asyncExec(`${targetDir}${target.pip} install Django > /dev/null 2>&1`);
   return;
 }
 
@@ -50,8 +55,8 @@ export async function os(options, platform) {
   } else {
     target.path = `/${envName}/bin/activate`;
     target.osVar = `/${envName}`;
-    target.pythonExecutable = `${envName}/bin/python`;
-    target.pip = `${envName}/bin/pip`;
+    target.pythonExecutable = `/${envName}/bin/python3`;
+    target.pip = `/${envName}/bin/pip3`;
     target.requirements = '/requirements.txt';
   }
 }
