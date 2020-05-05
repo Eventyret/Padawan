686268b0582ec5a371616dafcc33bade3451a3ac
Eventyret <simen.dehlin@gmail.com>
2020-05-05 19:49:37 +0100

    Added comments and changed url

--- src/generate/generateHTML.js
+++ src/generate/generateHTML.js
@@ -1,9 +1,12 @@
+
+/**
+ * This will generate the html for Milestone 1 and Milestone 2
+ * @param {Object} config - Configuration options
+ */
 export async function generateHTML(config) {
   const script = config.template.js  ? `<script src="assets/js/app.js"></script>`: '';
-  const head = `
-  <!DOCTYPE html>
+  const head = `<!DOCTYPE html>
   <html lang="en">
-  
   <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,15 +22,14 @@ export async function generateHTML(config) {
   <main class="container">
 	<section class="row">
 		<div class="col-12">
-			<img src="https://i.imgur.com/zgnEzrJ.png" class="img-fluid mx-auto d-block" alt="Padwan tool"/>
+			<img src="https://upload.wikimedia.org/wikipedia/en/d/d7/Ahsoka_Tano.png" class="img-fluid mx-auto d-block" alt="Padwan tool"/>
 			<h2 class="text-center">Thank you for using the Padwan tool to setup your <b>${config.name}</b> Project</h2>
 		</div>
 	</section>
 </main>
   `;
 
-  const html = `
-  ${head}
+  const html = `${head}
   <body>
   ${extra}
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
@@ -35,7 +37,6 @@ export async function generateHTML(config) {
   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
   ${script}
 </body>
-
 </html>`;
 
   return html;
