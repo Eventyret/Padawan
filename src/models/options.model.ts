08d7bdbb727b95c8f744e84c9c232558aaa91770
Eventyret <simen.dehlin@gmail.com>
2020-05-20 19:31:15 +0100

    Added templates and userOptions

--- src/models/options.model.ts
+++ src/models/options.model.ts
@@ -1,3 +1,18 @@
 interface UserOptions {
-	
+  name: [];
+  git: boolean;
+  template: Templates;
+  clean: boolean;
+  gitpod: boolean;
+  env: boolean;
+  envName: string;
+  createENV: boolean;
+}
+
+interface Templates {
+  name: string;
+  python: boolean;
+  django: boolean;
+  flask: boolean;
+  js: boolean;
 }
